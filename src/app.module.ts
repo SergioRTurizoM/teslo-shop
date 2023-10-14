94583e17ce63c3d2b270827695fd831252d5612d
SergioRTurizoM <>
2023-10-13 21:32:58 -0500

    Manejo de archivos e imagenes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,29 +4,36 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
-    
     ConfigModule.forRoot(),
-    
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.HOST,
-      port: +process.env.PORT,
+      port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
     }),
-    
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
     ProductsModule,
-    
+
     CommonModule,
-    
-    SeedModule
-  
+
+    SeedModule,
+
+    FilesModule,
   ],
   controllers: [],
   providers: [],
